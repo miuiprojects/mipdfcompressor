99572f339d15a9293f9cd469d0177b2932449c3a
miuiprojects <80858743+miuiprojects@users.noreply.github.com>
2022-01-01 09:37:10 +0530

    Delete callback.py

--- callback.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import CallbackQuery
-
-
-@Client.on_callback_query(filters.regex(r'^close_btn$'))
-async def close_button(c, cb: CallbackQuery):
-    await cb.message.delete()
